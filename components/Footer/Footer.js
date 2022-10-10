9c8cd31c8caa837fbce4219e0a118cecaf898062
maweiche <maweiche@gmail.com>
2022-10-10 14:37:04 -0700

    added icons to footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,13 +2,37 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "./Footer.module.css";
+import { LogoTwitter , LogoGithub , LogoInstagram, LogoLinkedin } from 'react-ionicons';
 
 export default function Footer() {
   const router = useRouter();
   
   return (
     <div className={styles.footer_container}>
-        FOOTER...
+        <LogoGithub
+          color='white'
+          height="5vh"
+          width="5vw"
+          onClick={() => router.push('https://github.com/maweiche')}
+        />
+        <LogoTwitter
+          color='white'
+          height="5vh"
+          width="5vw"
+          onClick={() => router.push('https://twitter.com/topshotturtles')}
+        />
+        <LogoInstagram
+          color='white'
+          height="5vh"
+          width="5vw"
+          onClick={() => router.push('https://www.instagram.com/matt_weichel/')}
+        />
+        <LogoLinkedin
+          color='white'
+          height="5vh"
+          width="5vw"
+          onClick={() => router.push('https://www.linkedin.com/in/mattweichel/')}
+        />
     </div>
   );
 }
--- components/Projects/ProjectsDisplay.js
+++ components/Projects/ProjectsDisplay.js
@@ -12,26 +12,36 @@ export default function Projects(obj) {
     return (
         <div className={styles.project_card}>
             <div className={styles.project_title}>
-                {project.name}
-            </div>
-            <div className={styles.project_description}>
-                <p>
-                    {project.description}
-                </p>
-            </div>
-            <div className={styles.project_link}>
                 <Link href={project.url}>
                     <a target="_blank">
                         {project.name}
                     </a>
-                </Link>
+                </Link> 
             </div>
-            <div className={styles.project_tags}>
-                {project.tags.map((tag, index) => (
-                    <div key={index} className={styles.project_tag}>
-                        {tag}
+            <div className={styles.project_card_body}>
+                <div className={styles.project_card_left}>
+                    <div className={styles.project_card_image}>
+                        <img src={project.image} alt={project.title} />
                     </div>
-                ))}
+                    </div>
+                <div className={styles.project_card_right}>
+                    
+                    <div className={styles.project_description}>
+                        <p>
+                            {project.description}
+                        </p>
+                    </div>
+                    <div className={styles.project_link}>
+                        
+                    </div>
+                    <div className={styles.project_tags}>
+                        {project.tags.map((tag, index) => (
+                            <div key={index} className={styles.project_tag}>
+                                {tag}
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
